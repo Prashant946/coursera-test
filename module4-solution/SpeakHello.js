5417fe9ff975729664d68d27bb60eda3b05618e0
Prashant946 <46805026+Prashant946@users.noreply.github.com>
2019-02-14 21:05:32 +0530

    FinalSolution

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,4 +1,4 @@
-function() {// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+(function() {// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
 
@@ -22,4 +22,4 @@ helloSpeaker.speak= function(name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 window.helloSpeaker = helloSpeaker;
-}(window);
+})(window);
